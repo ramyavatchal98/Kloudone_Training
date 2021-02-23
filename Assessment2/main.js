347d2a5cc353e2cca39d114945c12a33154d4b44
ramya <ramyabvatchal98@gmail.com>
2021-02-24 00:34:09 +0530

    --

--- Assessment2/main.js
+++ Assessment2/main.js
@@ -1,3 +1,20 @@
-function toggle() {
-  document.getElementsById("nav").style.width=50px;
+var result= document.getElementById("submit");
+result.addEventListener("onclick",Results);
+
+var row = 1;
+
+function Results() {
+  var firstName = document.getElementById("Fname").value;
+  var LastName= document.getElementById("Lname").value;
 }
+
+var display = document.getElementById("display");
+var newRow = display.insertRow(row);
+
+var cell1 = newRow.insertCell(0);
+var cell2 = newRow.insertCell(1);
+
+cell1.innerHTML= firstname;
+cell2.innerHTML = Lastname;
+
+row++;
