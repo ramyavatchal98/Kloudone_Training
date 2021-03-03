3df3e0fa21c5ef4b9ec5bfe9ca1362c78dbb0f33
ramya <ramyabvatchal98@gmail.com>
2021-03-03 20:01:10 +0530

    --

--- Database_Div/main.js
+++ Database_Div/main.js
@@ -1,31 +1,4 @@
 
-fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/small.json").then (
-  res=>{
-    res.json().then(
-      data =>{
-        console.log(data);
-        if(data.length > 0) {
-          var temp ="";
-          //for loop
-          data.forEach((u)=>{
-            temp += "<tr> ";
-            temp += "<td>"+u.id+"</td>";
-            temp += "<td>"+u.firstName+"</td>";
-            temp += "<td>"+u.lastName+"</td>";
-            temp += "<td>"+u.location+"</td>";
-            temp += "<td>"+u.date+"</td>";
-            temp += "<td>"+u.salary+"</td></tr>";
-
-          })
-
-          document.getElementById("data").innerHTML = temp;  
-          
-
-        }
-      }
-    )
-  }
-)
 fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/medium.json").then (
   res=>{
     res.json().then(
@@ -42,40 +15,9 @@ fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/medium.json")
             temp += "<td>"+u.location+"</td>";
             temp += "<td>"+u.date+"</td>";
             temp += "<td>"+u.salary+"</td></tr>";
-
           })
-
-          document.getElementById("data").innerHTML = temp;  
-          
-
-        }
-      }
-    )
-  }
-)
-fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/large.json").then (
-  res=>{
-    res.json().then(
-      data =>{
-        console.log(data);
-        if(data.length > 0) {
-          var temp ="";
-          //for loop
-          data.forEach((u)=>{
-            temp += "<tr> ";
-            temp += "<td>"+u.id+"</td>";
-            temp += "<td>"+u.firstName+"</td>";
-            temp += "<td>"+u.lastName+"</td>";
-            temp += "<td>"+u.location+"</td>";
-            temp += "<td>"+u.date+"</td>";
-            temp += "<td>"+u.salary+"</td></tr>";
-
-          })
-
-          document.getElementById("data").innerHTML = temp;  
-          
-
-        }
+           document.getElementById("data").innerHTML = temp;  
+         }
       }
     )
   }
@@ -87,4 +29,4 @@ var changeFontStyle = function (font) {
               = font.value; 
   }
 
- 
+  
