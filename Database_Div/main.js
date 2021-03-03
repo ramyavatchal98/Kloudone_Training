bf3688559a9b9951b3e500cbd47d251b7d35f259
ramya <ramyabvatchal98@gmail.com>
2021-03-03 17:36:25 +0530

    DB

---
+++ Database_Div/main.js
@@ -0,0 +1,28 @@
+
+fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/large.json").then (
+  res=>{
+    res.json().then(
+      data =>{
+        console.log(data);
+        if(data.length > 0) {
+          var temp ="";
+          //for loop
+          data.forEach((u)=>{
+            temp += "<tr> ";
+            temp += "<td>"+u.id+"</td>";
+            temp += "<td>"+u.firstName+"</td>";
+            temp += "<td>"+u.lastName+"</td>";
+            temp += "<td>"+u.location+"</td>";
+            temp += "<td>"+u.date+"</td>";
+            temp += "<td>"+u.salary+"</td></tr>";
+
+          })
+
+          document.getElementById("data").innerHTML = temp;  
+          
+
+        }
+      }
+    )
+  }
+)
