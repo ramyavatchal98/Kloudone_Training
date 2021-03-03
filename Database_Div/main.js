34296ca75a385f6ab486ece9dec6b519c84f33b4
ramya <ramyabvatchal98@gmail.com>
2021-03-03 18:34:56 +0530

    --

--- Database_Div/main.js
+++ Database_Div/main.js
@@ -1,4 +1,58 @@
 
+fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/small.json").then (
+  res=>{
+    res.json().then(
+      data =>{
+        console.log(data);
+        if(data.length > 0) {
+          var temp ="";
+          //for loop
+          data.forEach((u)=>{
+            temp += "<tr> ";
+            temp += "<td>"+u.id+"</td>";
+            temp += "<td>"+u.firstName+"</td>";
+            temp += "<td>"+u.lastName+"</td>";
+            temp += "<td>"+u.location+"</td>";
+            temp += "<td>"+u.date+"</td>";
+            temp += "<td>"+u.salary+"</td></tr>";
+
+          })
+
+          document.getElementById("data").innerHTML = temp;  
+          
+
+        }
+      }
+    )
+  }
+)
+fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/medium.json").then (
+  res=>{
+    res.json().then(
+      data =>{
+        console.log(data);
+        if(data.length > 0) {
+          var temp ="";
+          //for loop
+          data.forEach((u)=>{
+            temp += "<tr> ";
+            temp += "<td>"+u.id+"</td>";
+            temp += "<td>"+u.firstName+"</td>";
+            temp += "<td>"+u.lastName+"</td>";
+            temp += "<td>"+u.location+"</td>";
+            temp += "<td>"+u.date+"</td>";
+            temp += "<td>"+u.salary+"</td></tr>";
+
+          })
+
+          document.getElementById("data").innerHTML = temp;  
+          
+
+        }
+      }
+    )
+  }
+)
 fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/large.json").then (
   res=>{
     res.json().then(
@@ -26,3 +80,11 @@ fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/large.json").
     )
   }
 )
+
+var changeFontStyle = function (font) { 
+  document.getElementById( 
+      "data").style.fontSize 
+              = font.value; 
+  }
+
+ 
